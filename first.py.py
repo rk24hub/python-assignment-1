7187b5a44459a748172cf0a253ee1705a20a50b6
rk24hub <Rupanshikalra5360@gmail.com>
2025-07-23 15:46:50 +0530

    Add files via upload

---
+++ first.py.py
@@ -0,0 +1,16 @@
+# task 1 perform mathematical operations
+
+a =int(input("enter the first number"))
+b =int(input("enter the second number"))
+
+print("addition = ", a+b)
+print("subtraction = ", a-b)
+print("multiplication = ",a*b)
+print("division = ", a%b)
+
+
+# task 2 perform the
+
+first = input("what is your first name")
+last  =  input("what is your last name")
+print("hello,",first,last , "! welcome to the python program")
